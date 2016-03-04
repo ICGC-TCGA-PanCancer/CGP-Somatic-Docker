8594bea0061d47f103e7d865cb8be1c9bce12108
Keiran M Raine <keiranmraine@gmail.com>
2016-03-04 09:48:08 +0000

    Can't test for something that doesn't exist yet, and should test directory rather than just exists

--- scripts/run_sanger.sh
+++ scripts/run_sanger.sh
@@ -1,26 +1,27 @@
 #! /bin/bash
-set -x
+set -eu
 # If /datastore/work.ini exists then it will be executed
 # this should be done by the docker mount -v option
 # if not found will default to the in-build version that will attempt to run the test data
 #  - If test data is not found it will attempt to download it
 
-target_path=`find /home/seqware/CGP-Somatic-Docker/target -type d -name 'Workflow_Bundle_CgpSomaticCore_*SeqWare_*'`
 ini='/home/seqware/CGP-Somatic-Docker/workflow/config/CgpSomaticCore.ini'
 user_ini='/datastore/work.ini'
 
-if [ -e $user_ini ]; then
+if [ -f $user_ini ]; then
   echo "PREP: Found user defined ini file at $user_ini";
   ini=$user_ini
 else
   echo -e "PREP: No user defined ini file at $user_ini\n\tRunning test config: $ini";
-  if [ !-e '/datastore/HCC1143_ds' ]; then
+  if [ ! -d "/datastore/HCC1143_ds" ]; then
     echo "PREP: test data not found, downloading...";
     curl -sSL https://s3-eu-west-1.amazonaws.com/wtsi-pancancer/testdata/HCC1143_ds.tar | tar -C /datastore -x
   fi
 fi
 
 mvn clean install
+
+target_path=`find /home/seqware/CGP-Somatic-Docker/target -type d -name 'Workflow_Bundle_CgpSomaticCore_*SeqWare_*'`
 
 /home/seqware/bin/seqware bundle launch \
   --dir $target_path \
