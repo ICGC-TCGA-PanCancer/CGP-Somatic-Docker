2e776e32f145d4e8992a5568c555b396e11b7e84
Denis Yuen <denis.yuen@oicr.on.ca>
2016-11-22 17:27:08 -0500

    Fiddle with environment variables

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,9 @@ set -x
 
 gosu root chmod a+wrx /tmp
 gosu root chmod a+wrx /var/spool/cwl
+export TMPDIR=/tmp
+export HOME=/var/spool/cwl
+env
 gosu root bash -c "$*"
 #allow cwltool to pick up the results created by seqware
 gosu root chmod -R a+wrx  /var/spool/cwl
